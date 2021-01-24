bdcfbb6bcde208d237875c24665bd285aa2c99e3
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-01-24 17:25:28 +0000

    test4

--- main.py
+++ main.py
@@ -17,15 +17,18 @@ def updateStatsSection(path, data):
 	updateFileInfo(path,'STATS',string)
 
 if __name__=='__main__':
-	print("Args: {}".format(len(sys.argv)))
 	access_token = sys.argv[1]
 	if access_token:
 		g = Github(access_token)
-		user = g.get_user()
-		data=getActivityPercentage(user)
-		#h=[100, 76, 84, 64, 24, 3, 7, 3, 4, 23, 33, 42, 24, 60, 87, 136, 159, 171, 127, 88, 100, 77, 126, 112]
-		#data = [round(100*val/sum(h)) for val in h]
-		updateStatsSection('README.md',data)
+		if g:
+			user = g.get_user()
+			data=getActivityPercentage(user)
+			updateStatsSection('README.md',data)
+		else:
+			print("Error with login: {}".format(g))
+	else:
+		print("Error with args: {}".format(sys.argv[1]))
+
 		
 		
 
